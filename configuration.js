03507e477d085d9a5fbcc4e0708d64c022a2a445
mathieu-farosole <mathieu.faro@icloud.com>
2024-11-05 15:00:22 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -1,52 +1,23 @@
 const config = {
-  name: "Guillaume Levrier",
+  name: "Mathieu Faro Sole",
   description: {
     EN: {
-      short: `Guillaume Levrier is a political scientist working on biotechnology, currently a CNRS postdoc for the
+      short: `Mathieu Faro Sole is conducting a PhD in political science named "prefets and water management", 
+      affiliated to the BlueState projetc. He is conducting his thesis at the
         <a href="https://cordis.europa.eu/project/id/951393" target="_blank">Nanobubbles ERC grant</a>. He is affiliated
         to the <a href="http://umr-lisis.fr/membre/guillaume-levrier/" target="_blank">Laboratoire Interdisciplinaire
-          Sciences Innovations Sociétés (LISIS)</a>, and is a research associate to <a
-            href="https://www.sciencespo.fr/cevipof/en/researcher/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> and to the French National Library (BnF). `,
-      long: "",
+          Sciences Innovations Sociétés (LISIS)</a>.
     },
     FR: {
-      short: `Guillaume Levrier est un chercheur en sciences
-        politiques
-        spécialisé sur les biotechnologies, actuellement postdoc CNRS pour le <a
-          href="https://cordis.europa.eu/project/id/951393" target="_blank">projet
-          ERC Nanobubbles</a>. Il est affilié au <a
-            href="http://umr-lisis.fr/membre/guillaume-levrier/"
+      short: `Mathieu Faro Sole est un doctorant en sciences politiques
+        conduisant une thèse intitulée "les préfets et la gestion de l'eau", intégrée au projet BlueState. Il est affilié au <a
+            href="http://umr-lisis.fr/membre/mathieu-farosole/"
             target="_blank">Laboratoire Interdisciplinaire
           Sciences
-          Innovations Sociétés (LISIS)</a>, est associé au <a
-            href="https://www.sciencespo.fr/cevipof/fr/chercheur/guillaume-levrier.html"
-            target="_blank">CEVIPOF (Sciences Po)</a> et à la Bibliothèque nationale de
-        France (BnF).`,
-      long: "",
-    },
+          Innovations Sociétés (LISIS)</a>.
   },
   links: [
-    {
-      label: "twitter",
-      value: "@g_levrier",
-      url: "https://twitter.com/g_levrier",
-    },
-    {
-      label: "google scholar",
-      value: "profile",
-      url: "https://scholar.google.com/citations?user=IRiJFNMAAAAJ&hl=en",
-    },
-    {
-      label: "pubpeer",
-      value: "profile",
-      url: "https://pubpeer.com/search?q=guillaume+levrier",
-    },
-    {
-      label: "linked in",
-      value: "profile",
-      url: "https://www.linkedin.com/in/guillaume-levrier-phd-89156838/",
-    },
+   
   ],
-  email: { name: "guillaume.levrier", domain: "politique.science" },
+  email: { name: "mathieu.farosole", domain: "politique.science" },
 };
