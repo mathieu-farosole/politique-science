4c10b5d8006665aff8138e6f85ed0d96e0b37df9
mathieu-farosole <mathieu.faro@icloud.com>
2024-11-05 15:52:08 +0100

    Update configuration.js

--- configuration.js
+++ configuration.js
@@ -3,13 +3,13 @@ const config = {
   description: {
     EN: {
       short: `Mathieu Faro Sole is conducting a PhD in political science named "prefets and water management", 
-      affiliated to the BlueState project. He is conducting his thesis at the
+      affiliated to the BlueState project. <br> He is conducting his thesis at the
       <a href="http://umr-lisis.fr/" target="_blank">Laboratoire Interdisciplinaire
           Sciences Innovations Sociétés (LISIS)</a>.`
     },
     FR: {
       short: `Mathieu Faro Sole est un doctorant en sciences politiques
-        conduisant une thèse intitulée "les préfets et la gestion de l'eau", intégrée au projet BlueState. Il est affilié au <a
+        conduisant une thèse intitulée "les préfets et la gestion de l'eau", intégrée au projet BlueState. <br> Il est affilié au <a
             href="http://umr-lisis.fr/"
             target="_blank">Laboratoire Interdisciplinaire
           Sciences
